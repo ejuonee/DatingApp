bc40c694ecbab74ba30250c8c8d3933cec68d2ce
Olamide Ekoko <Olamide.Ekoko@stanbicibtc.com>
2021-06-29 16:19:25 +0100

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using DatingApp.Data_Transfer_Object;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataTransferObject _context;
+        public UsersController(DataTransferObject context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+             
+        }
+
+        //api/user/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+             
+        }
+    }
+}
---
+++ Data Transfer Object/DataTransferObject.cs
@@ -0,0 +1,18 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data_Transfer_Object
+{
+    public class DataTransferObject : DbContext
+    {
+        public DataTransferObject( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
