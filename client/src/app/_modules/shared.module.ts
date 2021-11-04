c62b8968fd247aad8eed4c322e158a8ae52acd4c
Olamide Ekoko <Olamide.Ekoko@stanbicibtc.com>
2021-11-04 10:20:26 +0100

    End Of Section (

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -4,6 +4,8 @@ import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {TabsModule} from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 
 
@@ -14,13 +16,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule,
     ReactiveFormsModule,
     BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({ positionClass: 'toast-bottom-right' })
+    ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }),
+    TabsModule.forRoot(),
+    NgxGalleryModule
   ],
   exports: [
     NgbModule,
     ReactiveFormsModule,
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule,
+    NgxGalleryModule
     ]
 })
 export class SharedModule { }
